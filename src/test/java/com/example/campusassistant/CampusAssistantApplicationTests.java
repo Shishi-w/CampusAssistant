ed0f818b483289811b5df1f5a46e8bb3e77a7d8f
Shishi <241250064@smail.nju.edu.cn>
2026-04-12 13:26:06 +0800

    Initial commit: Campus Assistant project setup

---
+++ src/test/java/com/example/campusassistant/CampusAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.campusassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
